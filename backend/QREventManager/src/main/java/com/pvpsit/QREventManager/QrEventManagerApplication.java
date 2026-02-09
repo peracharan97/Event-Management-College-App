8eb099a7a0a88fd396743380b6272e1b8fadfa4d
peracharan97 <peracharan97@gmail.com>
2026-02-09 14:48:05 +0530

    Initial commit - frontend and backend setup

---
+++ backend/QREventManager/src/main/java/com/pvpsit/QREventManager/QrEventManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pvpsit.QREventManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrEventManagerApplication.class, args);
+	}
+
+}
---
+++ backend/QREventManager/src/test/java/com/pvpsit/QREventManager/QrEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvpsit.QREventManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
